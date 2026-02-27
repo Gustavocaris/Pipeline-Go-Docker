6f96b1118f4b706c45d6e399fbff0067c7a74a99
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-02-27 11:56:12 -0300

    chore: fix docker compose setup and database connectivity
    - Debugged container DNS resolution
    - Resolved postgres volume/version conflict
    - Fixed Go redeclaration error
    - Ensured successful app and database startup

--- database/db.go
+++ database/db.go
@@ -2,22 +2,33 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
+var DB *gorm.DB
 
 func ConectaComBancoDeDados() {
 	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	var err error
+
+	for i := 0; i < 10; i++ {
+		DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
+		if err == nil {
+			log.Println("Conectado ao banco com sucesso!")
+			break
+		}
+
+		log.Println("Banco ainda não está pronto, tentando novamente...")
+		time.Sleep(2 * time.Second)
+	}
+
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panic("Erro ao conectar com banco de dados após várias tentativas")
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
